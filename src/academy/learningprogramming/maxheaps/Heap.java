a02eee065d2c24aad96b45b0e13b53ca318d05c0
Tootin0730 <yisan1004@gmail.com>
2023-06-26 18:45:24 +0900

    Heaps (Delete)

--- src/academy/learningprogramming/maxheaps/Heap.java
+++ src/academy/learningprogramming/maxheaps/Heap.java
@@ -13,11 +13,25 @@ public class Heap {
         if (isFull()) {
             throw new IndexOutOfBoundsException("Heap is full");
         }
-
         heap[size] = value;
-
         fixHeapAbove(size);
         size++;
+    }
+
+    public int delete(int index) {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("Heap is empty");
+        }
+        int parent = getParent(index);
+        int deletedValue = heap[index];
+        heap[index] = heap[size - 1];
+        if (index == 0 || heap[index] < heap[parent]) {
+            fixHeapBelow(index, size - 1);
+        } else {
+            fixHeapAbove(index);
+        }
+        size--;
+        return deletedValue;
     }
 
     private void fixHeapAbove(int index) {
@@ -29,11 +43,49 @@ public class Heap {
         heap[index] = newValue;
     }
 
+    private void fixHeapBelow(int index, int lastHeapIndex) {
+        int childToSwap;
+
+        while (index <= lastHeapIndex) {
+            int leftChild = getChild(index, true);
+            int rightChild = getChild(index, false);
+            if (leftChild <= lastHeapIndex) {
+                if (rightChild > lastHeapIndex) {
+                    childToSwap = leftChild;
+                } else {
+                    childToSwap = (heap[leftChild] > heap[rightChild] ? leftChild : rightChild);
+                }
+                if (heap[index] < heap[childToSwap]) {
+                    int tmp = heap[index];
+                    heap[index] = heap[childToSwap];
+                    heap[childToSwap] = tmp;
+                } else {
+                    break;
+                }
+                index = childToSwap;
+            } else {
+                break;
+            }
+        }
+    }
+
+    public void printHeap() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(heap[i]);
+            System.out.print(", ");
+        }
+        System.out.println();
+    }
     public boolean isFull() {
         return size == heap.length;
     }
-
     public int getParent(int index) {
         return (index - 1) / 2;
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public int getChild(int index, boolean left) {
+        return 2 * index + (left ? 1 : 2);
     }
 }
