a94e26db7d984de42d7c77582814a94698a891d1
Tootin0730 <yisan1004@gmail.com>
2023-06-27 12:01:54 +0900

    Heapsort

--- src/academy/learningprogramming/maxheaps/Main.java
+++ src/academy/learningprogramming/maxheaps/Main.java
@@ -5,30 +5,27 @@ import java.util.PriorityQueue;
 public class Main {
 
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        Heap heap = new Heap(10);
 
-        pq.add(30);
-        pq.add(-22);
-        pq.add(1343);
-        pq.add(54);
-        pq.add(0);
-        pq.add(3492);
-        pq.add(429);
+        heap.insert(80);
+        heap.insert(75);
+        heap.insert(60);
+        heap.insert(68);
+        heap.insert(55);
+        heap.insert(40);
+        heap.insert(52);
+        heap.insert(67);
 
-//        System.out.println(pq.peek());
-//        System.out.println(pq.remove());
-//        System.out.println(pq.peek());
-//        System.out.println(pq.poll());
-//        System.out.println(pq.peek());
-//        System.out.println(pq.remove());
+        heap.printHeap();
 
-        Object[] ints = pq.toArray();
-        for(Object num: ints){
-            System.out.println(num);
-        }
+        //System.out.println(heap.peek());
 
-//        System.out.println(pq.peek());
-        pq.add(-1);
-//        System.out.println(pq.peek());
+        heap.delete(0);
+        heap.printHeap();
+
+        //System.out.println(heap.peek());
+
+        heap.sort();
+        heap.printHeap();
     }
 }
